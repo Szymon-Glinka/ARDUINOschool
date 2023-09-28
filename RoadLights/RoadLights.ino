dee87065f77b53c97cbde02759474427d5b8c44c
Szymon Glinka <131162335+Szymon-Glinka@users.noreply.github.com>
2023-09-28 11:13:39 +0200

    Update RoadLights.ino

--- RoadLights/RoadLights.ino
+++ RoadLights/RoadLights.ino
@@ -1,20 +1,66 @@
 void setup() {
- for(int i=1; i<=11; i++){
-   pinMode(i, OUTPUT);
- }
+  for(int i=1; i<=11; i++){
+    pinMode(i, OUTPUT);
+  }
   pinMode(12, INPUT_PULLUP);
   pinMode(13, INPUT_PULLUP);
 }
 
 void loop() {
-  STREATgreen_red(6, 4, 2, 7, 5, 3);
-  delay(4000);  
-  STREATgreen_red(7, 5, 3, 6, 4, 2);
-  delay(4000);
+  WALKred(8, 9);
+  STREETred(5, 6, 7);
+  delay(250);
+  STREETgreen(2, 3, 4);
+  WALKgreen(10, 11);
+  delay(2000);
 
+  WALKred(10, 11);
+  STREETred(2, 3, 4);
+  delay(250);
+  STREETgreen(5, 6, 7);
+  WALKgreen(8, 9);
+  delay(2000);
 }
 
-void STREATgreen_red(int R1, int Y1, int G1, int R2, int Y2, int G2){
+void STREETred(int R, int Y, int G){
+  //digitalWrite(G, HIGH);
+  //delay(1000);
+  digitalWrite(Y, HIGH);
+  digitalWrite(G, LOW);
+  delay(1000);
+  digitalWrite(R, HIGH);
+  digitalWrite(Y, LOW);
+}
+
+void STREETgreen(int R, int Y, int G){
+  digitalWrite(R, HIGH);
+  delay(1000);
+  digitalWrite(Y, HIGH);
+  delay(1000);  
+  digitalWrite(R, LOW);
+  digitalWrite(Y, LOW);
+  digitalWrite(G, HIGH);
+}
+
+void WALKgreen(int R, int G){
+  digitalWrite(R, LOW);
+  delay(250);
+  digitalWrite(G, HIGH);
+}
+
+void WALKred(int R, int G){
+  for(int i=1; i<=5; i++){
+    digitalWrite(G, LOW);
+    delay(250);
+    digitalWrite(G, HIGH);
+    delay(250);
+  }
+  digitalWrite(G, LOW);
+  delay(250);
+  digitalWrite(R, HIGH);
+}
+
+void STREETgreen_red(int R1, int Y1, int G1, int R2, int Y2, int G2){
   digitalWrite(G2, HIGH);
   delay(1000);
   digitalWrite(Y2, HIGH);
@@ -32,24 +78,4 @@ void STREATgreen_red(int R1, int Y1, int G1, int R2, int Y2, int G2){
   digitalWrite(R1, LOW);
   digitalWrite(Y1, LOW);
   digitalWrite(G1, HIGH);
-}
-
-void STREATred(int R, int Y, int G){
-  digitalWrite(G, HIGH);
-  delay(1000);
-  digitalWrite(Y, HIGH);
-  digitalWrite(G, LOW);
-  delay(1000);
-  digitalWrite(R, HIGH);
-  digitalWrite(Y, LOW);
-}
-
-void STREATgreen(int R, int Y, int G){
-  digitalWrite(R, HIGH);
-  delay(1000);
-  digitalWrite(Y, HIGH);
-  delay(1000);  
-  digitalWrite(R, LOW);
-  digitalWrite(Y, LOW);
-  digitalWrite(G, HIGH);
 }
