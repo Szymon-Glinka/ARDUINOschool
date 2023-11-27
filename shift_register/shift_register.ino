79706f333d159ec4e4c6bd7c63a84083cc2daacc
Szymon Glinka <131162335+Szymon-Glinka@users.noreply.github.com>
2023-11-27 15:47:44 +0100

    Add files via upload

--- shift_register/shift_register.ino
+++ shift_register/shift_register.ino
@@ -1,17 +1,45 @@
-#define data 3
-#define latch 4
-#define shift 5
+#define data 4
+#define latch 3
+#define shift 2
 
-int test1 = 0b0101010101010101;
+byte tab1[] = {
+            0b11111111,
+            0b10101011,
+            0b10101011,
+            0b11111111,
+            0b01111101,
+            0b10111011,
+            0b11000111,
+            0b11111111,
+            };
 
-void setup() {
+byte tab2[] = {
+            0b10000000,
+            0b01000000,
+            0b00100000,
+            0b00010000,
+            0b00001000,
+            0b00000100,
+            0b00000010,
+            0b00000001,
+            };
+int index = 0;
 
-  for(int i=3; i<6; i++) pinMode(i, OUTPUT);
+void setup(){
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
 
 }
 
-void loop() {
-  digitalWrite(latch, LOW);
-  shiftOut(data, shift, MSBFIRST, test1);
-  digitalWrite(latch, HIGH);
+void loop(){
+  for(int i=0; i<8; i++){
+    digitalWrite(latch, LOW);
+    shiftOut(data, shift, MSBFIRST, tab1[i]);
+    shiftOut(data, shift, MSBFIRST, tab2[i]);
+    digitalWrite(latch, HIGH);
+    delay(1);
+  }
 }
+
+
